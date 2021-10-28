2ec03f26d97548b3c351300c9566752a86494f08
Oliver <oliverosorio@gmail.com>
2021-10-28 10:44:41 -0400

    learing.js

--- learning.js
+++ learning.js
@@ -12,5 +12,13 @@ console.log("843-581-0000")
 console.log(843-581-0000)
 let y = 3;
 let cl = console.log;
+ (let count = 1; count < 5; count++) {console.log(count);}
+
+ for (let index = 0; index++) < array.length; 10index++) {
+     cl (index * 10); 
+     
+ }
+    
+}
 
 
